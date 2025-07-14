b9fd1bb9a6524bb3c46c335d5ed3942280d7c784
João Pedro Fiorindo <joaopedrofiorindopinto@gmail.com>
2025-07-14 00:18:56 -0300

    criação da rota /login com autenticação simulada
    Criação da dependencia cors, Adição da rota POST /login para a validação de email e senha.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3001;
 
-app.use(express.json());
+app.use(cors()); // permite requisições do front
+app.use(express.json()); // permite ler JSON no corpo da requisição
 
+// Rota de teste
 app.get('/', (req, res) => {
-  res.send('API está funcionando 🚀');
+  res.send('API está rodando 🚀');
+});
+
+// Rota de login
+app.post('/login', (req, res) => {
+  const { email, senha } = req.body;
+
+  // Simulação de autenticação
+  if (email === 'admin@teste.com' && senha === '1234') {
+    res.json({
+      sucesso: true,
+      usuario: {
+        nome: 'Admin',
+        email: 'admin@teste.com',
+        token: 'fake-jwt-token-123',
+      },
+    });
+  } else {
+    res.status(401).json({ sucesso: false, mensagem: 'Credenciais inválidas' });
+  }
 });
 
 app.listen(PORT, () => {
