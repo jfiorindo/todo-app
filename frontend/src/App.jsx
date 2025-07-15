81fa86800252b9f0862b8542909545ff6688c0cd
João Pedro Fiorindo <joaopedrofiorindopinto@gmail.com>
2025-07-14 22:44:34 -0300

    adição tela de tarefas
    Adicionei a tela de tarefas provisória para ajustar a integração com o backend, e assim que a integração estiver pronta iniciaremos as novas funcionalidades.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Login from './components/login';
+import Tarefas from './components/tarefas';
 
 function App() {
   const [usuario, setUsuario] = useState(null);
